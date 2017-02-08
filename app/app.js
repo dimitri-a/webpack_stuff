bd8026198506f2172d5bf262cdc742c4cee6b8de
Dimitri Couwenberg <couwenberg@gmail.com>
2017-02-08 12:43:15 +1100

    _react2.default.render is not a function

--- app/app.js
+++ app/app.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import {render} from 'react-dom'
+import ReactDOM from 'react-dom';
 
-
-class Application extends React.Component {
-    render() {
-        return <div>
-            <h1>Hello, ES6 and React 0.13!</h1>
-            <p>
-
-            </p>
-        </div>;
-    }
-}
-
-/*
- * Render the above component into the div#app
- */
-React.render(<Application />, document.getElementById('app'));
+ReactDOM.render(
+   <div>hello world</div>,
+    document.getElementById('root')
+);
